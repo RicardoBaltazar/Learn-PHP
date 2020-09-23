a1a978e17cfc3b14556d22058dfd3dc440d0cd18
RicardoBaltazar <ricardo.baltazar04@gmail.com>
2020-09-23 20:27:24 -0300

    iniciando projeto para estudo

---
+++ fundamentos/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fundamentos</title>
+</head>
+
+<body>
+
+
+    <?php
+
+        echo 'Ola Mundo <br/>'; //é um contrutor da linguagem
+        print 'Ola Mundo <br/>'; //print é função, tem um valor de retorno de 1 para que ele possa ser usado em expressõe
+
+
+        
+
+
+
+
+
+    ?>
+
+</body>
+
+</html>
