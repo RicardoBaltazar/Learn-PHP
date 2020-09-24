fd55e7ef6ee44cac312950b7f3da8cfc89e7f2ee
RicardoBaltazar <ricardo.baltazar04@gmail.com>
2020-09-24 13:53:07 -0300

    registrando dados no mysql

--- twitter_clone/registra_usuario.php
+++ twitter_clone/registra_usuario.php
@@ -1,11 +1,27 @@
 <?php
 
-echo $_POST['usuario'];
-echo '<br>';
-echo $_POST['email'];
-echo '<br>';
-echo $_POST['senha'];
-echo '<br>';
+    require_once('db.class.php');
 
+    $usuario = $_POST['usuario'];
+    '<br>';
+    $email =  $_POST['email'];
+    echo '<br>';
+    $senha = $_POST['senha'];
+    echo '<br>';
+
+
+    $objDb = new db();
+    $link = $objDb->conecta_mysql();
+
+    //montando a query de insert
+    $sql = "insert into usuarios(usuario, email, senha)values('$usuario', '$email', '$senha')";
+
+    //executar a query
+    //também criando tratativa de erro caso não execute a query
+    if( mysqli_query($link, $sql)){
+        echo 'usuario registrado com sucesso';
+    } else {
+        echo 'erro ao registrar o usuario';
+    }
 
 ?>
