41fa830205042b25b11ccd6a875f7dfdb247b9a4
RicardoBaltazar <ricardo.baltazar04@gmail.com>
2020-09-25 17:14:18 -0300

    acessando arrays dos dados

---
+++ twitter_clone/teste_mysqli.php
@@ -0,0 +1,29 @@
+<?php
+
+    require_once('db.class.php');
+
+    $sql = "SELECT * FROM usuarios ";
+
+    $objDb = new db();
+    $link = $objDb->conecta_mysql();
+
+    //executar a cunsulta
+    $resultado_id = mysqli_query($link, $sql);
+
+    if($resultado_id){
+        $dados_usuario = array();
+        
+        while($linha = mysqli_fetch_array($resultado_id, MYSQLI_ASSOC)) {
+            $dados_usuario[] = $linha;
+        };
+    
+        foreach($dados_usuario as $usuario){
+            var_dump($usuario);
+            echo '<br><br>';
+        };
+
+    } else {
+        echo 'erro na consulta';
+    }
+
+?>
